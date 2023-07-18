47cdd23ae9057d0f87a1014caf741a8d49bb590d
Mickaël Schoentgen <contact@tiger-222.fr>
2023-07-18 14:42:31 +0200

    chore: simplify local server code

--- server.py
+++ server.py
@@ -1,19 +1,10 @@
-import locale
-import sys
-
-from paste import httpserver
-
-from host.app import application
-
-
-def main():
-    """Main logic."""
-
-    # Set the locale to the system one for number formatting
-    locale.setlocale(locale.LC_ALL, "")
-
-    httpserver.serve(application, host="0.0.0.0", port=9999)
-
+"""
+Local web server for testing purposes.
+"""
 
 if __name__ == "__main__":
-    sys.exit(main())
+    from paste import httpserver
+
+    from host.app import application
+
+    httpserver.serve(application, host="0.0.0.0", port=9999)
